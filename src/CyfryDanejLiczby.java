34c0e532292bcc0b58d75e5aea749dbbb1af040a
commilitio <51202678+commilitio@users.noreply.github.com>
2023-10-27 14:04:18 +0200

    edited CyfryDanejLiczby.java

--- src/CyfryDanejLiczby.java
+++ src/CyfryDanejLiczby.java
@@ -6,17 +6,22 @@
  */
 
 
+import java.util.Arrays;
+
 public class CyfryDanejLiczby {
     public static void main(String[] args) {
 
-        int liczba = 764678643;                             // liczba na ktorej pracuje
-
-        String liczbaStr = String.valueOf(liczba);          // przeksztalcam ją w String
-
-        char[] tablica = liczbaStr.toCharArray();           /* .toCharArray() pobiera poszczegolne char w
-                                                  danym String;  zapisuje te char do tablicy typu char[]  */
+        int liczba = 764678643;                                         // liczba na ktorej pracuje
 
 
+        String wynik = String.valueOf(liczba);                          // przeksztalcam ją w String
+        System.out.println(Arrays.toString((wynik.toCharArray())));
+
+                                        // lub
+
+
+        char[] tablica = wynik.toCharArray();           /* .toCharArray() pobiera poszczegolne char w
+                                                           danym String;  zapisuje te char do tablicy typu char[]  */
 
         for (int i = 0; i < tablica.length; i++) {
             System.out.print(tablica[i] + " ");        // lub System.out.println(tablica[i]);    << println
